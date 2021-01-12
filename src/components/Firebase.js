26d87fefd553e1fa7928eea79dcb68f42b016670
sahzad <sahjad.ranchi@gmail.com>
2021-01-12 12:54:40 +0530

    initial commit

---
+++ src/components/Firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZyni8I9vDxeZfcnH_cfY1N4597cZp_9g",
+  authDomain: "mothersfood-a1de8.firebaseapp.com",
+  databaseURL: "https://mothersfood-a1de8.firebaseio.com",
+  projectId: "mothersfood-a1de8",
+  storageBucket: "mothersfood-a1de8.appspot.com",
+  messagingSenderId: "801490700383",
+  appId: "1:801490700383:web:e13622c82e1d29a196b3ea",
+  measurementId: "G-QKDQ3PGXJJ",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.firestore().settings({ timestampsInSnapshots: true})
+
+export default firebase;
