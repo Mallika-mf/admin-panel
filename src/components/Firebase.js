ba84551e9b65a723652993ff7bb925c87ad00c6a
sahzad <sahjad.ranchi@gmail.com>
2021-01-12 13:00:36 +0530

    initial commit2

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -3,18 +3,16 @@ import "firebase/firestore";
 import "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBZyni8I9vDxeZfcnH_cfY1N4597cZp_9g",
-  authDomain: "mothersfood-a1de8.firebaseapp.com",
-  databaseURL: "https://mothersfood-a1de8.firebaseio.com",
-  projectId: "mothersfood-a1de8",
-  storageBucket: "mothersfood-a1de8.appspot.com",
-  messagingSenderId: "801490700383",
-  appId: "1:801490700383:web:e13622c82e1d29a196b3ea",
-  measurementId: "G-QKDQ3PGXJJ",
+var firebaseConfig = {
+  apiKey: "AIzaSyBtBTV7LJ6ch2CyakL2iIpppVWjNlXYOWk",
+  authDomain: "mothersfoodtesting.firebaseapp.com",
+  databaseURL: "https://mothersfoodtesting.firebaseio.com",
+  projectId: "mothersfoodtesting",
+  storageBucket: "mothersfoodtesting.appspot.com",
+  messagingSenderId: "537550768022",
+  appId: "1:537550768022:web:21711009b79025b75a3fb7",
+  measurementId: "G-G9BL0Q6QQM",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// firebase.firestore().settings({ timestampsInSnapshots: true})
-
-export default firebase;
+firebase.analytics();
