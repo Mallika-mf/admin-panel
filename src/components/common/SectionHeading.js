58b6913208033e4444c9d6b23b244665772ea4d0
sahzad <sahjad.ranchi@gmail.com>
2021-02-03 14:31:56 +0530

    merged2

--- src/components/common/SectionHeading.js
+++ src/components/common/SectionHeading.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types'; 
+import React from "react";
+import PropTypes from "prop-types";
 
 class SectionHeading extends React.Component {
-	render() {
-    	return (
-            <div className={`section-header ${this.props.alignClass}`}>
-               <h2>{this.props.heading}</h2>
-               {this.props.subHeading ? (
-               		<p>{this.props.subHeading}</p>
-               		):''
-               }
-               <span className="line"></span>
-            </div>
-		);
-	}
+  render() {
+    return (
+      <div className={`section-header ${this.props.alignClass}`}>
+        <h2 style={{ color: "red" }}>{this.props.heading}</h2>
+        {this.props.subHeading ? <p>{this.props.subHeading}</p> : ""}
+        <span className="line"></span>
+      </div>
+    );
+  }
 }
-
 
 SectionHeading.propTypes = {
   heading: PropTypes.string.isRequired,
@@ -23,8 +19,8 @@ SectionHeading.propTypes = {
   alignClass: PropTypes.string,
 };
 SectionHeading.defaultProps = {
-  subHeading: '',
-  alignClass: 'text-center',
-}
+  subHeading: "",
+  alignClass: "text-center",
+};
 
 export default SectionHeading;
